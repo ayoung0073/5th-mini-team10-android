6bbf3fe1f79b696ae81da5ff7c073b0b2363c4f0
whk06061 <63198157+whk06061@users.noreply.github.com>
2020-09-24 21:08:26 +0900

    챌린지 목록 프래그먼트로 수정

--- app/src/main/java/com/moonayoung/greenlife/challenge/MainActivity.java
+++ app/src/main/java/com/moonayoung/greenlife/challenge/MainActivity.java
@@ -26,13 +26,13 @@ public class MainActivity extends AppCompatActivity {
     private FragmentChallenge fragmentChallenge = new FragmentChallenge();
     private FragmentFeed fragmentFeed = new FragmentFeed();
     private FragmentSetting fragmentSetting = new FragmentSetting();
-    RecyclerView challengeListView;
-    ChallengeAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.frameLayout, fragmentChallenge).commitAllowingStateLoss();
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
@@ -55,48 +55,15 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-        challengeListView = findViewById(R.id.challengListView); // 챌린지 목록 보여줄 리싸이클러뷰
 
-        fragmentManager = getSupportFragmentManager();
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false); //linearlayout으로 리싸이클러뷰 설정
-        challengeListView.setLayoutManager(layoutManager);
+
+
 
         Intent intent = new Intent(this, LoadingActivity.class);
         startActivityForResult(intent, 101);
 
-        ChallengeList challengeList = new ChallengeList();
-        adapter = new ChallengeAdapter();
-//        adapter.setItems(challengeList.getChallengeLists()); // 데이터 저장되어 있음 Title, content, 세부 챌린지 배열
-        challengeListView.setAdapter(adapter); // 어댑터에 설정 -> 리싸이클러뷰에 챌린지 목록 보임
 
-        adapter.setOnItemClickListener(new onChallengeListClickListener() {
-            @Override
-            public void onItemClick(ChallengeAdapter.ViewHolder holder, View view, int position) { // 챌린지 목록 각각 눌렀을 때
-                ChallengeList item = adapter.getItem(position);
-                position++;
-                //Toast.makeText(getApplicationContext(), position + "번째 목록 선택됨",Toast.LENGTH_LONG).show();
-
-                switch (position) {
-                    case 1: // 첫번째 click
-                        fragmentManager.beginTransaction().replace(R.id.mainContainer, new ChallengeFragment1()).commit(); //1번째 챌린지 프래그먼트 띄움
-                        break;
-                    case 2:
-                        fragmentManager.beginTransaction().replace(R.id.mainContainer, new ChallengeFragment2()).commit();
-                        break;
-                    case 3:
-                        fragmentManager.beginTransaction().replace(R.id.mainContainer, new ChallengeFragment3()).commit();
-                        break;
-                    case 4:
-                        fragmentManager.beginTransaction().replace(R.id.mainContainer, new ChallengeFragment4()).commit();
-                        break;
-                    case 5:
-                        fragmentManager.beginTransaction().replace(R.id.mainContainer, new ChallengeFragment5()).commit();
-                        break;
-                }
-
-            }
-        });
 
     }
 
