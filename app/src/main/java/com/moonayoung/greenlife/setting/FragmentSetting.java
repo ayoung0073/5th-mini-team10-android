848eed30790607d76794a011f282724b13596927
hiroong <kjimin63@naver.com>
2020-09-29 17:52:00 +0900

    설정화면

--- app/src/main/java/com/moonayoung/greenlife/setting/FragmentSetting.java
+++ app/src/main/java/com/moonayoung/greenlife/setting/FragmentSetting.java
@@ -1,5 +1,6 @@
 package com.moonayoung.greenlife.setting;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,6 +13,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.moonayoung.greenlife.R;
 import com.moonayoung.greenlife.challenge.MainActivity;
@@ -21,12 +24,13 @@ import org.w3c.dom.Text;
 public class FragmentSetting extends Fragment {
     private Button buttonAccount;
     private Button buttonNotice;
-    /*
+    private Activity activity;
+
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
         activity= (MainActivity) getActivity();
-    }*/
+    }
 
     @Nullable
 
@@ -36,26 +40,30 @@ public class FragmentSetting extends Fragment {
         // Inflate the layout for this fragment
 
         View view = inflater.inflate(R.layout.fragment_setting, container, false);
-        view.findViewById(R.id.btn_accountSetting).setOnClickListener((View.OnClickListener) this); //계정설정
-        view.findViewById(R.id.btn_notice).setOnClickListener((View.OnClickListener) this); //공지사항
-        view.findViewById(R.id.txt_question).setOnClickListener((View.OnClickListener) this); //문의하기
-        view.findViewById(R.id.txt_version).setOnClickListener((View.OnClickListener) this); //버전정보
 
-        buttonAccount.setOnClickListener(new View.OnClickListener() {
+        buttonAccount = (Button)view.findViewById(R.id.btn_accountSetting);
+
+        buttonAccount.setOnClickListener(new View.OnClickListener() { //계정설정 버튼 눌렀을 때 accountSetting activity로 전환
             @Override
             public void onClick(View view) {
+                //getActivity().startActivity(new Intent(requireActivity(), AccountSettings.class));
+                //ft=manager.beginTransaction();
+                //ft.replace(R.id.)
+                /*((AccountSettings)getActivity()).findViewById(R.id.btn_accountSetting);
+                activity.onFragmentChange(0);*/
+
                 Intent intent = new Intent(getActivity(),AccountSettings.class);
-                startActivity(intent);
+                //startActivity(intent);
             }
         });
 
-        /*buttonNotice.setOnClickListener(new View.OnClickListener() {
+       /* buttonNotice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });*/
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        return view;
     }
 }
 
