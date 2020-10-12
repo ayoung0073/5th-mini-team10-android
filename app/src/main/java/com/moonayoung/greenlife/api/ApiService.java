b4bebaaddfbb547f4e458f2316c7ac5ac9f16e44
ayoung0073 <69340410+ayoung0073@users.noreply.github.com>
2020-10-12 17:30:08 +0900

    사진업로드 틀

--- app/src/main/java/com/moonayoung/greenlife/api/ApiService.java
+++ app/src/main/java/com/moonayoung/greenlife/api/ApiService.java
@@ -42,8 +42,8 @@ public interface ApiService {
     @PUT("/challenge") // 요청 바디로(참여버튼) -> 해당챌린지Id 보냄
     Call<Challenge> putData(@Body Challenge param,@HeaderMap HashMap<String,String> map);
 
-    @POST("/photo") // 사진업로드 // 아직 사진객체 ?
-    Call<String> postPhoto(@Body Bitmap param);
+    @POST("feed/upload") // 사진업로드 // 아직 사진객체 ?
+    Call<UploadPost> postPhoto(@Header("token") String token, @Body Bitmap param);
 
     @GET("/feed") // feed 확인
     Call<Feed> getFeed();
