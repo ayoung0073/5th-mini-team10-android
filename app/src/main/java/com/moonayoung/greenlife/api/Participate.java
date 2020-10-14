dda3d04f06b68653d25a42c216838849248a3e24
whk06061 <63198157+whk06061@users.noreply.github.com>
2020-10-14 16:58:33 +0900

    세부챌린지통신

---
+++ app/src/main/java/com/moonayoung/greenlife/api/Participate.java
@@ -0,0 +1,28 @@
+package com.moonayoung.greenlife.api;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+public class Participate {
+    @SerializedName("success")
+    private boolean success;
+    @SerializedName("count")
+    private int count;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
